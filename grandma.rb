631e1296c2d532aba64738e82ba5d054d08fe7d5
Matt Thomas <matthew.keith.thomas@gmail.com>
2020-09-08 22:37:59 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -11,3 +11,15 @@
 
 # However if you say 'I LOVE YOU GRANDMA!', she should respond with
 # 'I LOVE YOU TOO PUMPKIN!'
+
+
+  name = "I LOVE YOU GRANDMA!"
+
+  if name == "I LOVE YOU GRANDMA!"
+   puts "I LOVE YOU TOO PUMPKIN!"
+  elsif name == name.upcase
+   puts "NO, NOT SINCE 1938!"
+  else 
+    puts "HUH? SPEAK UP, SONNY!"
+  
+  end
