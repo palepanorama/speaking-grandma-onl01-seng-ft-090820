7a843812505090135631d856ebcecae0be1fb5aa
Matt Thomas <matthew.keith.thomas@gmail.com>
2020-09-08 22:38:42 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,9 +1,10 @@
 # Write a speak_to_grandma method.
-def speak_to_grandma
+def speak_to_grandma(phrase)
 
 # Whatever you say to grandma, she should respond with
 # HUH?! SPEAK UP, SONNY!
 # unless you shout it (type in all capitals).
+if phrase = 
 
 # If you shout, she can hear you (or at least she thinks so) 
 # and yells back
