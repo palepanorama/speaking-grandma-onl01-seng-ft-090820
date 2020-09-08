276e24d2ba8e107c27e7e5fd860c22b2a209ca03
Matt Thomas <matthew.keith.thomas@gmail.com>
2020-09-08 22:41:12 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -19,4 +19,5 @@ def speak_to_grandma(phrase)
     return "I LOVE YOU TOO PUMPKIN!"
   elsif phrase == phrase.downcase 
     return "HUH? SPEAK UP, SONNY!"
-  elsif phrase = 
+  elsif phrase = phrase.upcase
+    return "NO"
